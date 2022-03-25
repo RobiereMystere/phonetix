2a1871b57cd107d154cd6e53fe46f5e6ef96bb41
RobiereMystere <robiere.mystere@gmail.com>
2022-03-25 13:45:27 +0100

    serializer

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,5 +117,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 django_heroku.settings(locals())
